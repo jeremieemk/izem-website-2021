8bf674c8b8c606da1ae0a85688a39593188246d6
jeremie <jeremieemk@gmail.com>
2022-03-01 16:40:21 +0100

    updated dependencies

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "34b0ab51acc1a797dbd3";
+/******/ 	var hotCurrentHash = "e7a758adb48f424f0d93";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
