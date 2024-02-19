97b386686e56fa090f77d6c250b2f1a6a47115b1
Jérémie <jeremieemk@gmail.com>
2024-02-19 18:40:01 +0100

    new look

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b60e054e3a39b560"; }
+/******/ 		__webpack_require__.h = function() { return "2eea18a978bee1fd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
